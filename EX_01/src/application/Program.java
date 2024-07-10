907763ee00157820ad91d7ab4de841b7f665d49a
Tchauperas <121253173+Tchauperas@users.noreply.github.com>
2024-07-10 09:17:44 -0300

    EX_01 done

--- EX_01/src/application/Program.java
+++ EX_01/src/application/Program.java
@@ -18,7 +18,26 @@ public class Program {
         System.out.print("Quantity on  stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product);
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number to added in stock: ");
+
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number to remove from the stock: ");
+
+        int deploy = sc.nextInt();
+        product.removeProducts(deploy);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
 
         sc.close();
     }
