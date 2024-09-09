7d71961807d97df1d9cf8bb83a991d6a1751ae8a
Tchauperas <marcos.vhynycyus@gmail.com>
2024-09-09 14:46:30 -0300

    commit

---
+++ _wsJava/EX_04/src/entities/ImportedProduct.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ImportedProduct extends Product {
+	private double customsFee;
+	
+	public ImportedProduct() {}
+
+	public ImportedProduct(String name, double price, double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	
+
+}
