72b17a542818993cfb2c40a3f8706e867a5aa5c6
Marcos_Vhynycyus <marcos.vhynycyus@gmail.com>
2024-09-30 15:13:24 -0300

    save

--- excecoes_personalizadas/src/application/Program.java
+++ excecoes_personalizadas/src/application/Program.java
@@ -1,7 +1,26 @@
 package application;
 
+import java.util.Scanner;
+import java.util.Date;
+import java.util.Locale;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int rn = sc.nextInt();
+        System.out.print("Check-in date (DD/MM/YYYY): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (DD/MM/YYYY): ");
+        Date checkOut = sdf.parse(sc.next());
+
         
+
+        sc.close();
     }
 }
