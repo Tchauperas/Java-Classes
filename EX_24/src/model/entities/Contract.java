456785a59175a0040d70b37a7277b48d6bc665ed
Marcos_Vhynycyus <marcos.vhynycyus@gmail.com>
2024-10-15 10:10:37 -0300

    commit

---
+++ EX_24/src/model/entities/Contract.java
@@ -0,0 +1,41 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Contract {
+    
+    private Integer number;
+    private LocalDate date;
+    private Double totalValue;
+
+    private ArrayList<Installment> installments = new ArrayList<>();
+
+    public Contract(Integer number, LocalDate date, Double totalValue) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public ArrayList<Installment> getInstallments() {
+        return installments;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    public Double getTotalValue() {
+        return totalValue;
+    }
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+}
