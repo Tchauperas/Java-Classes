c1afcdba05d19a5352faf673fe5f77d7a117b0bc
Marcos_Vhynycyus <marcos.vhynycyus@gmail.com>
2024-10-22 11:24:42 -0300

    commit

---
+++ generics/src/model/services/PrintService.java
@@ -0,0 +1,31 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<TypeSafe> {
+    
+    private List<TypeSafe> list = new ArrayList<>();
+
+    public void addValue(TypeSafe value) {
+        list.add(value);
+    }
+
+    public TypeSafe first() {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        } 
+        return list.get(0);
+    }
+
+    public void print() {
+        System.out.print("[");
+        if(!list.isEmpty()) {
+            System.out.print(list.get(0));
+        }
+        for (int i = 1; i < list.size(); i++) {
+            System.out.print(", " + list.get(i));
+        }
+        System.out.println("]");
+    }
+}
