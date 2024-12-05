3591bcc6fefb02759ee5bab97da5a9763d2ebe76
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2024-12-05 13:30:28 -0300

    commit

--- EX_26/src/application/Program.java
+++ EX_26/src/application/Program.java
@@ -3,28 +3,49 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
-        
-        String path = "C://temp//in.txt";
-        Map<String, Integer> votes = new TreeMap<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
+	public static void main(String[] args) throws ParseException {
 
-            while(line != null) {
-                String[] fields = line.split(",");
-                String name = fields[0];
-                String vote = fields[1];
-                int intVote = Integer.parseInt(vote);
+		Scanner sc = new Scanner(System.in);
 
-                votes.put(name, intVote);
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
+		Map<String, Integer> votes = new LinkedHashMap<>();
+
+		String path = "C:\\Users\\marcos.silva\\Documents\\My Projects\\Java-Classes\\EX_26\\assets\\in.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int count = Integer.parseInt(fields[1]);
+
+				if (votes.containsKey(name)) {
+					int votesSoFar = votes.get(name);
+					votes.put(name, count + votesSoFar);
+				}
+				else {
+					votes.put(name, count);
+				}
+				
+				line = br.readLine();
+			}
+			
+			for (String key : votes.keySet()) {
+				System.out.println(key + ": " + votes.get(key));
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		sc.close();
+	}
 }
