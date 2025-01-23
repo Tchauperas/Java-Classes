611fd8af138ea0fdc33a8730b28c55336db7215f
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-01-23 09:44:27 -0300

    commit

--- insert_data/src/application/Program.java
+++ insert_data/src/application/Program.java
@@ -5,21 +5,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
 
+            /* 
             st = conn.prepareStatement(
                     "insert into seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
@@ -32,6 +33,10 @@ public class Program {
             st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4, 300000.00);
             st.setInt(5, 4);
+            */
+
+            st = conn.prepareStatement("insert into department (Name) values ('Schoolar'), ('Homecare')",
+            Statement.RETURN_GENERATED_KEYS);
 
             int rowsAffected = st.executeUpdate();
 
@@ -47,8 +52,6 @@ public class Program {
 
         } catch (SQLException e) {
             System.out.println("An error occured: " + e.getMessage());
-        } catch (ParseException e) {
-            e.printStackTrace();
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
