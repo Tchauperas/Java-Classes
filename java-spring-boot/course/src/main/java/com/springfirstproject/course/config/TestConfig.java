5067a91ccc2d05dbcf21a001973d83e4ea6d04b0
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-02-18 22:06:28 -0300

    commit

--- java-spring-boot/course/src/main/java/com/springfirstproject/course/config/TestConfig.java
+++ java-spring-boot/course/src/main/java/com/springfirstproject/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.springfirstproject.course.entities.Order;
 import com.springfirstproject.course.entities.User;
+import com.springfirstproject.course.entities.enums.OrderStatus;
 import com.springfirstproject.course.repositories.OrderRepository;
 import com.springfirstproject.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Clare", "clare@gmail.com", "9999999", "123456");
         User u2 = new User(null, "Bob", "bob@gmail.com", "98999999", "321654");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.CANCELED);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3)); 
