3cfb6216f827d8be06a237a2bd1b003b8d1c23b6
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-02-24 11:48:53 -0300

    commit

--- java-spring-boot/course/src/main/java/com/springfirstproject/course/config/TestConfig.java
+++ java-spring-boot/course/src/main/java/com/springfirstproject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.springfirstproject.course.entities.Category;
 import com.springfirstproject.course.entities.Order;
 import com.springfirstproject.course.entities.OrderItem;
+import com.springfirstproject.course.entities.Payment;
 import com.springfirstproject.course.entities.Product;
 import com.springfirstproject.course.entities.User;
 import com.springfirstproject.course.entities.enums.OrderStatus;
@@ -78,6 +79,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 }
