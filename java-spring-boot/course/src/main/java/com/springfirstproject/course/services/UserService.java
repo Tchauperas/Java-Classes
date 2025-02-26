bbcdc8da78194e4851bd4441198ca8e46dfd3bbb
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2025-02-26 09:21:31 -0300

    commit

--- java-spring-boot/course/src/main/java/com/springfirstproject/course/services/UserService.java
+++ java-spring-boot/course/src/main/java/com/springfirstproject/course/services/UserService.java
@@ -13,9 +13,11 @@ import com.springfirstproject.course.repositories.UserRepository;
 import com.springfirstproject.course.services.exceptions.DatabaseException;
 import com.springfirstproject.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
 
@@ -43,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     public void updateData(User entity, User obj) {
